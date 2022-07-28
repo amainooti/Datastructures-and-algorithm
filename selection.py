be0fdfc65574a87499c586d786e2d2e3ab6053a3
amainooti <amainooti@gmail.com>
2022-07-28 10:26:43 +0100

    selection sort algorithm included.

--- selection.py
+++ selection.py
@@ -7,25 +7,45 @@
 # find the smallest number
 
 
-def find_smallest(arr):
-    smallest = arr[0]
-    smallest_index = 0
-    for item in range(1, len(arr)):
-        if arr[item] < smallest:
-            smallest = arr[item]
-            smallest_index = item
-    return smallest_index
+# def find_smallest(arr):
+#     smallest = arr[0]
+#     smallest_index = 0
+#     for item in range(1, len(arr)):
+#         if arr[item] < smallest:
+#             smallest = arr[item]
+#             smallest_index = item
+#     return smallest_index
 
 
 # section sort
 
 
+# def selection_sort(arr):
+#     newArr = []
+#     for i in range(len(arr)):
+#         smallest = find_smallest(arr)
+#         newArr.append(arr.pop(smallest))
+#     return newArr
+
+
+def smallest(arr):
+    smallest_element = arr[0]
+    smallest_index = 0
+    for item in range(len(arr)):
+        if arr[item] < smallest_element:
+            smallest_element = arr[item]
+            smallest_index = item
+    return smallest_index
+
+
 def selection_sort(arr):
-    newArr = []
-    for i in range(len(arr)):
-        smallest = find_smallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
+    new_arr = []
+    for item in range(len(arr)):
+        smallest_item = smallest(arr)
+        # remove the arr from from the original arr
+        removed_list = arr.pop(smallest_item)
+        new_arr.append(removed_list)
+    return new_arr
 
 
 arr = [12, 10, 13, 8, 9, 14]
